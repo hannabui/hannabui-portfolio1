5c5d6f3d5078a58e29d3c63b9907aa2abcbe836f
hannabui <97701564+hannabui@users.noreply.github.com>
2022-03-18 20:08:03 -0400

    Update index.js

--- portfolio1/exercise/src/components/RepetitionExercise/index.js
+++ portfolio1/exercise/src/components/RepetitionExercise/index.js
@@ -1,13 +1,38 @@
 import React, { useState } from "react";
 
+const buttonStyle = {
+    backgroundColor: 'lightPink',
+    width: '100%',
+    height: '100%',
+    border: 'white',
+    color: 'white',
+    padding: '10px 20px',
+    textDecoration: 'none',
+    borderRadius: '5px',
+    textAlign: 'center',
+    fontFamily: 'Georgia',
+    fontSize: '24px',
+    margin: '6px'
+  }
+  const textStyle = {
+    fontSize: '34px',
+    textAlign: 'center',
+    fontFamily: 'Georgia'
+}
 function Squats({ name }) {
     let [count, setCount] = useState(0)
     return (
         <>
-            <div>{name}</div>
-            <div>The rep count is {count}</div>
-            <button onClick={() => setCount(count => count + 1)}>Add rep</button>
-            <button onClick={() => setCount(count => 0)}>Reset</button>
+            <div style={textStyle}>{name}</div>
+            <div style={textStyle}>The rep count is {count}</div>
+            <button 
+            style={buttonStyle}
+            onClick={() => setCount(count => count + 1)}>Add rep
+            </button>
+            <button 
+            style={buttonStyle}
+            onClick={() => setCount(count => 0)}>Reset
+            </button>
         </>
     )
 }
